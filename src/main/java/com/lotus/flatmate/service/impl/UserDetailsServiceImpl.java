4384b7f1802193895a1912c63b0190de7b1f2f59
HtetOakkar <gemini.oakkar@gmail.com>
2023-10-31 11:20:26 +0630

    change(auth): email checking flow

--- src/main/java/com/lotus/flatmate/service/impl/UserDetailsServiceImpl.java
+++ src/main/java/com/lotus/flatmate/service/impl/UserDetailsServiceImpl.java
@@ -1,10 +1,14 @@
 package com.lotus.flatmate.service.impl;
 
+import java.util.Optional;
+
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.lotus.flatmate.emailVerification.entity.EmailVerification;
+import com.lotus.flatmate.emailVerification.repository.EmailVerificationRepository;
 import com.lotus.flatmate.model.exception.NonVerifiedException;
 import com.lotus.flatmate.model.exception.RecordNotFoundException;
 import com.lotus.flatmate.security.UserPrincipal;
@@ -18,15 +22,27 @@ import lombok.RequiredArgsConstructor;
 public class UserDetailsServiceImpl implements UserDetailsService {
 
 	private final UserRepository userRepository;
-	
+	private final EmailVerificationRepository emailVerificationRepository;
+
 	@Override
 	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-		User user = userRepository.findByEmail(email)
-				.orElseThrow(() -> new RecordNotFoundException("User account with email '" + email +"' not found."));
+		Optional<User> userOptional = userRepository.findByEmail(email);
+
 //		if (!user.getEmailVerification().isVerified() && user.getEmailVerification() == null) {
 //			throw new NonVerifiedException("You email is not verified yet. Please, verify your email first.");
 //		}
+		if (userOptional.isEmpty()) {
+			EmailVerification emailVerification = emailVerificationRepository.findByEmail(email).orElseThrow(
+					() -> new RecordNotFoundException("User account with email '" + email + "' does not exist!"));
+			if (!emailVerification.isVerified()) {
+				throw new NonVerifiedException("Your email is not verified. Please, verify your email first!");
+			}
+		}
+		
+		User user = userOptional.get();
+
 		return UserPrincipal.build(user);
+
 	}
 
 }
